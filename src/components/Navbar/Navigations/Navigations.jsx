d7936f5ed0a45a3a607327f56b5ffc4efc391f2f
Venkat <venkatsaimane@gmail.com>
2024-01-02 22:17:13 +0530

    Updated Fix for scroll

--- src/components/Navbar/Navigations/Navigations.jsx
+++ src/components/Navbar/Navigations/Navigations.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import styles from "./Navigations.module.css";
-import { Link } from "react-router-dom";
+import { Link } from "react-scroll";
 
 export default function NavBar() {
   return (
     <div className={styles.navigations}>
-      <Link to="/" className={styles.text}>
+      <Link to="home" smooth={true} duration={500} className={styles.text}>
         Home
       </Link>
-      <Link to="/" className={styles.text}>
+      <Link to="attorneys" smooth={true} duration={500} className={styles.text}>
         Attorneys
       </Link>
-      <Link to="/" className={styles.text}>
+      <Link to="practiceAreas" smooth={true} duration={500} className={styles.text}>
         Practice Areas
       </Link>
-      <Link to="/" className={styles.text}>
+      <Link to="about" smooth={true} duration={500} className={styles.text}>
         About Us
       </Link>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,14 +8,20 @@ import Contact from "../../components/Contact/Contact";
 
 export default function Home() {
   return (
-    <>
-      <Hero />
-      <div className={styles.wrapper}>
+    <div className={styles.wrapper}>
+      <section id="home">
+        <Hero className={styles.hero} />
+      </section>
+      <section id="attorneys">
         <Attorneys />
+      </section>
+      <section id="practiceAreas">
         <PracticeAreas />
+      </section>
+      <section id="about">
         <About />
         <Contact />
-      </div>
-    </>
+      </section>
+    </div>
   );
 }
